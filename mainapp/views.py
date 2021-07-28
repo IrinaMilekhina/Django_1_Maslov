2ab78bcae34ab191f799ba024c752c022ad734bf
IrinaMilekhina <i.mezhova@gmail.com>
2021-07-28 19:26:02 +0300

    Вывод динамического контента на страницах (элементы меню, список товара, заголовок страницы).

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,7 +7,17 @@ def main(request):
 
 
 def products(request):
-    content = {'title': 'Продукты'}
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
     return render(request, 'mainapp/products.html', content)
 
 
