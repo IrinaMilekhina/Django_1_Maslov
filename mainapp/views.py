92a081119ce533f058e41c6b532f8e17adf78d3c
IrinaMilekhina <i.mezhova@gmail.com>
2021-07-29 15:59:00 +0300

    1. Поработать с запросами в консоли через механизм Django ORM. 2. Реализовать механизм вывода товаров по категориям. 3. *Организовать динамическую генерацию меню категорий и подсветку выбранной категории. 4. Создать приложение корзины. 5. Реализовать механизм добавления товара в корзину. 6. Вывести в меню счетчик купленных товаров. 7. *Написать в модели корзины методы для определения общего количества и стоимости добавленных товаров. Вывести эти величины в меню вместо счетчика, сделанного на уроке.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,9 @@
 import json
 import os
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from basketapp.models import Basket
 from mainapp.models import Product, ProductCategory
 
 JSON_PATH = 'mainapp/json'
@@ -11,6 +12,13 @@ JSON_PATH = 'mainapp/json'
 def load_from_json(file_name):
     with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='utf-8') as infile:
         return json.load(infile)
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
 
 
 def main(request):
@@ -27,13 +35,38 @@ def main(request):
 
 
 def products(request, pk=None):
-    same_products = Product.objects.all()[:4]
+    title = 'продукты'
     links_menu = ProductCategory.objects.all()
+    basket = get_basket(request.user)
+
+    if pk is not None:
+        if pk == 0:
+            category = {
+                'pk': 0,
+                'name': 'все'
+            }
+            products = Product.objects.all().order_by('price')
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category=category)
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+        }
+        return render(request, 'mainapp/products_list.html', content)
+
+    some_products = Product.objects.all()[3:5]
     content = {
-        'title': 'Продукты',
+        'title': title,
         'links_menu': links_menu,
-        'same_products': same_products
+        'some_products': some_products,
+        'basket': basket,
     }
+
     return render(request, 'mainapp/products.html', content)
 
 
