0f077df13c0e007474b0a1f1994fb8b3fcb0b2ce
i.milekhina <i.mezhova@gmail.com>
2021-08-27 17:10:51 +0300

    оптимизация работы с заказом

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -24,9 +24,9 @@ class OrderItemForm(forms.ModelForm):
         model = OrderItem
         exclude = ()
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'form-control'
-                field.help_text = ''
-            self.fields['product'].queryset = Product.objects.all().select_related()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+        self.fields['product'].queryset = Product.objects.all().select_related()
