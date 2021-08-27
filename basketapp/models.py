c8f336eb374fa44d2d4c19671b20701e78cacee5
i.milekhina <i.mezhova@gmail.com>
2021-08-27 20:50:25 +0300

    Добавление @cached_property

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,7 +1,6 @@
+from django.utils.functional import cached_property
 from django.db import models
 from django.conf import settings
-from django.utils.functional import cached_property
-
 from mainapp.models import Product
 
 
@@ -22,31 +21,25 @@ class Basket(models.Model):
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
 
-    @cached_property
     def _get_product_cost(self):
-        "return cost of all products this type"
-        return self.product.price * self.quantity
+        return self.quantity * self.product.price
 
     product_cost = property(_get_product_cost)
 
     @cached_property
+    def get_items_cached(self):
+        return Basket.objects.filter(user=self.user).select_related()
+
     def _get_total_quantity(self):
-        "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user).select_related()
+        _items = self.get_items_cached
         _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
         return _totalquantity
 
     total_quantity = property(_get_total_quantity)
 
     def _get_total_cost(self):
-        "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
         return _totalcost
 
     total_cost = property(_get_total_cost)
-
-    # def delete(self):
-    #     self.product.quantity += self.quantity
-    #     self.product.save()
-    #     super().delete()
